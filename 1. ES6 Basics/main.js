9c32272c8c5ea87bd8fba0aa120b93372d06aa34
Sameejan1 <sameerjay1996@gmail.com>
2024-11-04 14:32:55 +0500

    Update main.js

--- 1. ES6 Basics/main.js
+++ 1. ES6 Basics/main.js
@@ -1,11 +1,11 @@
-function getUserName(userName) {
-  return userName;
+function retrieveFullName(fullName) {
+  return fullName;
 }
 
-let hasError = false;
-let isLoading = false;
+let isActive = true;
+let isComplete = true;
 
-console.log(hasError || getUserName("John Doe"));
+console.log(isActive || retrieveFullName("Alex Johnson"));
 
 //Template literals
 
@@ -16,147 +16,175 @@ console.log(firstName + " " + lastName, `${firstName} ${lastName}`);
 
 //Ternary operator
 
-const showDessert = true;
+const displayRecipeOne = true;
 
-function getDessertName(dessertName) {
-  return dessertName;
+function retrieveRecipeOneName(recipeTitle) {
+  return recipeTitle;
 }
 
-function getDrinkName(drinkName) {
-  return drinkName;
+function retrieveRecipeTwoName(recipeTitle) {
+  return recipeTitle;
 }
 
-showDessert
-  ? console.log(getDessertName("Cake"))
-  : console.log(getDrinkName("Coffee"));
+if (displayRecipeOne) {
+  console.log(retrieveRecipeOneName("Burger"));
+} else {
+  console.log(retrieveRecipeTwoName("Pepsi"));
+}
 
-const itemID = 2;
-const itemName = "Smartwatch";
-const itemRating = 4.5;
+displayRecipeOne
+  ? console.log(retrieveRecipeOneName("Burger"))
+  : console.log(retrieveRecipeTwoName("Pepsi"));
+
+const itemId = 2;
+const productTitle = "Product Samsung Galaxy";
+const reviewScore = 4;
 
 const item = {
-  id: itemID,
-  name: itemName,
-  rating: itemRating,
+  itemId,
+  productTitle,
+  reviewScore,
 };
 
 console.log(item);
 
 const item2 = {
-  description: "Description of Item 2",
-  ...item, // Spread operator to copy properties
+  description: "Item 2 details",
+  itemId,
+  productTitle,
+  reviewScore,
 };
 
-console.log(item2.description);
+const { description: itemDescription } = item2;
 
-const numbersArray = [5, 6, 7, 8];
+console.log(itemDescription);
 
-let getFirstValue = numbersArray[0];
-let getSecondValue = numbersArray[1];
+const numArray = [5, 6, 7, 8];
 
-console.log(getFirstValue, getSecondValue);
+let firstNum = numArray[0];
+let secondNum = numArray[1];
 
-const [firstElement, secondElement, thirdElement, fourthElement] = numbersArray;
+console.log(firstNum, secondNum);
 
-console.log(
-  firstElement,
-  secondElement,
-  thirdElement,
-  fourthElement
-);
+const [
+  numOne,
+  numTwo,
+  numThree,
+  numFour,
+] = numArray;
 
-//default parameters, spread and rest operators
+console.log(numOne, numTwo, numThree, numFour);
 
-function multiplyTwoNumbers(num1 = 3, num2 = 4) {
-  console.log(num1, num2);
-  return num1 * num2;
+//default parameters, spread, and rest operators
+
+function multiplyNumbers(factorOne = 3, factorTwo = 4) {
+  console.log(factorOne, factorTwo);
+  return factorOne * factorTwo;
 }
 
-console.log(multiplyTwoNumbers(15));
+console.log(multiplyNumbers(20));
 
-const colorArray = [10, 11, 12];
-const anotherColorArray = [13, 14, 15];
+const additionalArray = [8, 9, 10];
+const additionalArray2 = [13, 14, 15];
 
-console.log([2000, ...colorArray, 20, ...anotherColorArray, 3000]);
+console.log([1000, ...additionalArray, 80, ...additionalArray2, 20000]);
 
-function gatherInfo(identifier, ...remainingInfo) {
-  console.log(identifier, remainingInfo);
+function displayDetails(info, ...details) {
+  console.log(info, details);
 
-  //
-
-  return "Additional Information";
+  return "Alex Johnson";
 }
 
-console.log(gatherInfo("item-123", "data 1", "data 2", "data 3"));
+console.log(displayDetails(11, 12, 13, 14, 15, 16, 17, 18, 19));
 
 //map, filter, find, some, every, includes, indexOf, findIndex
 
-const people = [
+const peopleArray = [
   {
-    name: "Alice",
-    age: 25,
-    country: "Canada",
-  },
-  {
-    name: "Bob",
+    name: "Individual 01",
     age: 32,
-    country: "USA",
+    location: "Canada",
   },
   {
-    name: "Charlie",
-    age: 41,
-    country: "UK",
+    name: "Individual 02",
+    age: 25,
+    location: "Canada",
   },
   {
-    name: "David",
-    age: 54,
-    country: "Australia",
+    name: "Individual 03",
+    age: 37,
+    location: "France",
+  },
+  {
+    name: "Individual 04",
+    age: 45,
+    location: "Japan",
   },
 ];
 
-let getAllPeopleNames = people.map((person, index) => {
-  console.log(person, index);
-  return `${person.name} is ${person.age} years old`;
+let listNames = peopleArray.map((personDetail, index) => {
+  console.log(personDetail, index);
+  return `${personDetail.name} age is ${personDetail.age}`;
 });
 
-console.log(getAllPeopleNames);
+console.log(listNames);
 
-let getPersonFromCanada = people.find((person) => {
-  return person.country === "Canada";
+let personFromCanada = peopleArray.find((personDetail, index) => {
+  return personDetail.location === "Canada";
 });
 
-console.log(getPersonFromCanada);
+console.log(personFromCanada);
 
-let getAllPeopleFromUSA = people.filter((person) => {
-  return person.country === "USA";
+let allPeopleFromCanada = peopleArray.filter((personDetail, index) => {
+  return personDetail.location === "Canada";
 });
 
-console.log(getAllPeopleFromUSA);
+console.log(allPeopleFromCanada);
 
-let isThereAPersonAbove40 = people.some((person) => {
-  return person.age > 40;
+let isAgeAboveThirty = peopleArray.some((personDetail) => {
+  return personDetail.age > 30;
 });
 
-console.log(isThereAPersonAbove40);
+console.log(isAgeAboveThirty);
 
-let areAllPeopleAbove20 = people.every((person) => {
-  return person.age > 20;
+let isAgeAboveTwenty = peopleArray.every((personDetail) => {
+  return personDetail.age > 20;
 });
 
-console.log(areAllPeopleAbove20);
+console.log(isAgeAboveTwenty);
 
-const vegetables = ["tomato", "potato", "carrot"];
+const fruitBasket = ["grape", "mango", "berry"];
 
-console.log(vegetables.includes("broccoli"), vegetables.indexOf("broccoli"));
+console.log(fruitBasket.includes("Alex"), fruitBasket.indexOf("Alex"));
 
-let getIndexForPersonFromUK = people.findIndex((person) => {
-  return person.country === "UK";
+let indexFromFrance = peopleArray.findIndex((personDetail) => {
+  return personDetail.location === "Germany";
 });
 
-console.log(getIndexForPersonFromUK);
+console.log(indexFromFrance);
 
-let getProductListElement = document.querySelector(".product-list");
+let productListElement = document.querySelector(".products-list");
 
 function displayProducts(products) {
-  getProductListElement.innerHTML = products
-    .map((product) => `<li>${product.name
+  productListElement.innerHTML = products
+    .map((item) => `<p>${item.title}</p>`)
+    .join(" ");
+}
+
+async function fetchProductList() {
+  try {
+    const response = await fetch("https://dummyjson.com/products", {
+      method: "GET",
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+
+    if (data?.products?.length > 0) displayProducts(data?.products);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+fetchProductList();
